4cd8bc8bc179a9545ab7c921f3ccef483bc22163
BuildTools <unconfigured@null.spigotmc.org>
2019-09-16 21:46:12 +0300

    Tamamen TURKCE

---
+++ README.md
@@ -0,0 +1,13 @@
+-- Unkownlife.de
+
+
+## Installation
+
+1: Put esx_rent into your database
+2: put in your config "start Unknownlifecarrent"
+3: Put your vehicles into your shop!
+4: Don't Change the Name because the script will be broke
+
+5: Have fun ! :)
+
+Copyright
---
+++ locales/de.lua
@@ -0,0 +1,11 @@
+Locales['de'] = {
+	['shop'] = 'Araba Marketi',
+	['shops'] = 'Araba Marketleri',
+	['press_menu'] = 'Araba menüsünü açmak için ~INPUT_CONTEXT~ tuşuna bas.',
+	['bought'] = 'Az önce ~y~%sx~s~ ~b~%s~s~ aldın ve karşılığında ~r~$%s~s~ verdin.',
+	['not_enough'] = 'Yeterince ~r~paran yok~s~ İhtiyacın olan: %s',
+	['player_cannot_hold'] = 'Envanterinde yeterince boş yer yok!',
+	['shop_confirm'] = 'al %sx %s ve $%s ver?',
+	['no'] = 'hayır',
+	['yes'] = 'evet',
+}
